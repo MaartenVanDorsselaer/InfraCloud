4b2d9961b0e72d1b84fced298c81ca733acb1b5d
Sample User <maarten.vandorsselaer@student.odisee.be>
2021-12-19 13:19:17 +0000

    Changed port to 5080

--- jenkins/sample-app/sample-app.sh
+++ jenkins/sample-app/sample-app.sh
@@ -15,7 +15,10 @@ echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5080" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
+
 cd tempdir
+
 docker build -t sampleapp .
+
 docker run -t -d -p 5080:5080 --name samplerunning sampleapp
-docker container ls -a
+docker ps -a
--- jenkins/sample-app/tempdir/sample_app.py
+++ jenkins/sample-app/tempdir/sample_app.py
@@ -12,4 +12,4 @@ def main():
     return render_template("index.html" , datetime_now = datetime.datetime.now())
 
 if __name__ == "__main__":
-    microweb_app.run(host="localhost", port=5321)
+    microweb_app.run(host="0.0.0.0", port=5080)
